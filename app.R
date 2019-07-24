df0439503babcf609486adba1c735680308f5f67
wilsontom <wilsontom89@gmail.com>
2019-07-24 15:34:39 +0100

    try and configure local ports

--- app.R
+++ app.R
@@ -1,11 +1,15 @@
 
+library(shiny)
+options(shiny.host = '0.0.0.0')
+options(shiny.port = 3838)
+
 ui <- fluidPage(
    includeHTML('html/maintenance.html'),
  theme = shinythemes::shinytheme('yeti')
 
 )
 server <- function(input, output, session) {
-  options(shiny.port = 3838, shiny.host = "0.0.0.0")
+
 }
 
 shinyApp(ui = ui, server = server)
